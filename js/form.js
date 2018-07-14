25ca17cbfb6e0fb86ea4b1fd3b9dc0395c1ebfb4
helliany <ol.yurmanova@gmail.com>
2018-07-14 14:39:21 +0300

    Выполнила 8 задание - 4

--- js/form.js
+++ js/form.js
@@ -28,12 +28,18 @@
     palace: '10000'
   };
 
-  // поля формы заблокированы
+  // поля формы и селекты фильтра заблокированы
   var disableFields = function (value) {
     var fieldsets = form.querySelectorAll('fieldset');
+    var selects = filter.querySelectorAll('select');
+    var fieldsetFeature = filter.querySelector('.map__features');
     fieldsets.forEach(function (fieldset) {
       fieldset.disabled = value;
     });
+    selects.forEach(function (select) {
+      select.disabled = value;
+    });
+    fieldsetFeature.disabled = value;
   };
 
   // синхронизация типа жилья и цены
